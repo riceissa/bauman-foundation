b64aec913daa5a5536e2c37a9c2ac8ccaf0dbd48
Issa Rice <riceissa@gmail.com>
2017-12-30 21:25:11 -0800

    move stuff

--- scrape.py
+++ scrape.py
@@ -14,6 +14,39 @@ def main():
         go(writer)
 
 
+def do_grantee(fy_sums, grantee_num):
+    # Now we go to the grantee page to get information about each individual
+    # grant. We only need to do this once for each grantee.
+    if grantee_num not in fy_sums:
+        fy_sums[grantee_num] = {}
+        response_g = requests.get(grantee_url)
+        soup_g = BeautifulSoup(response_g.content, "lxml")
+        rows_g = soup_g.find_all("tr")[1:]  # Skip the header row of the table
+        for row_g in rows_g:
+            cols_g = row_g.find_all("td")
+
+            d = {}
+            d['grantee'] = grantee
+            d['grantee_url'] = grantee_url
+            d['fiscal_year'] = cols_g[0].text.strip()
+            d['amount'] = cols_g[1].text.strip()
+            d['grant_type'] = cols_g[2].text.strip()
+            d['grant_description'] = cols_g[3].text.strip()
+            writer.writerow(d)
+
+            amount_g = d['amount']
+            assert amount_g.startswith("$")
+            amount_g = float(amount_g.replace("$", "").replace(",", ""))
+            if d['fiscal_year'] not in fy_sums[grantee_num]:
+                fy_sums[grantee_num][d['fiscal_year']] = amount_g
+            else:
+                fy_sums[grantee_num][d['fiscal_year']] += amount_g
+
+        if not fy_sums[grantee_num]:
+            print("Failed to download grantee page:", grantee,
+                  grantee_num, file=sys.stderr)
+
+
 def go(writer):
     # This is a map: int -> fiscal_year -> float
     fy_sums = {}
@@ -37,37 +70,6 @@ def go(writer):
         amount = cols[1].text.strip()
         assert amount.startswith("$"), amount
         amount = float(amount.replace("$", "").replace(",", ""))
-
-        # Now we go to the grantee page to get information about each individual
-        # grant. We only need to do this once for each grantee.
-        if grantee_num not in fy_sums:
-            fy_sums[grantee_num] = {}
-            response_g = requests.get(grantee_url)
-            soup_g = BeautifulSoup(response_g.content, "lxml")
-            rows_g = soup_g.find_all("tr")[1:]  # Skip the header row of the table
-            for row_g in rows_g:
-                cols_g = row_g.find_all("td")
-
-                d = {}
-                d['grantee'] = grantee
-                d['grantee_url'] = grantee_url
-                d['fiscal_year'] = cols_g[0].text.strip()
-                d['amount'] = cols_g[1].text.strip()
-                d['grant_type'] = cols_g[2].text.strip()
-                d['grant_description'] = cols_g[3].text.strip()
-                writer.writerow(d)
-
-                amount_g = d['amount']
-                assert amount_g.startswith("$")
-                amount_g = float(amount_g.replace("$", "").replace(",", ""))
-                if d['fiscal_year'] not in fy_sums[grantee_num]:
-                    fy_sums[grantee_num][d['fiscal_year']] = amount_g
-                else:
-                    fy_sums[grantee_num][d['fiscal_year']] += amount_g
-
-            if not fy_sums[grantee_num]:
-                print("Failed to download grantee page:", grantee,
-                      grantee_num, file=sys.stderr)
 
         try:
             assert fy_sums[grantee_num][fiscal_year] == amount, \
