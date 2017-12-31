445747351e7626d6a883f5c356ce61a759b07cbb
Issa Rice <riceissa@gmail.com>
2017-12-30 19:15:47 -0800

    snapshot

--- scrape.py
+++ scrape.py
@@ -22,11 +22,10 @@ def go(writer):
     all_grants_url = "https://www.baumanfoundation.org/grants/search?amount=All&fiscal_year=&name=&items_per_page=All"
     response = requests.get(all_grants_url)
     soup = BeautifulSoup(response.content, "lxml")
-    rows = soup.find_all("tr")
-    next(rows)  # skip the header row of the table
+    rows = soup.find_all("tr")[1:]  # Skip the header row of the table
     for row in rows:
         d = {}
-        cols = row.find_all("td"):
+        cols = row.find_all("td")
         d['grantee'] = cols[0].text.strip()
         grantee_path = cols[0].find("a")["href"]
         grantee_num = int(grantee_path.split('/')[-1])
@@ -42,9 +41,9 @@ def go(writer):
         # page all over again)
         if grantee_num not in grantee_dict:
             grantee_dict[grantee_num] = []
-            response_g = requests.get(grantee_url)
+            response_g = requests.get(d['grantee_url'])
             soup_g = BeautifulSoup(response_g.content, "lxml")
-            rows_g = soup_g.find_all("tr")
+            rows_g = soup_g.find_all("tr")[1:]  # Skip the header row of the table
             for row_g in rows_g:
                 d_g = {}
                 cols_g = row_g.find_all("td")
@@ -59,9 +58,10 @@ def go(writer):
                if x['fiscal_year'] == d['fiscal_year'] and
                   x['amount'] == d['amount']]
         # There should be only one grant matching this
-        assert len(l_g) == 1, (grantee_num, l_g)
+        assert len(l_g) == 1, (d, grantee_dict[grantee_num], grantee_num, l_g)
         d['grant_type'] = l_g[0]['grant_type']
         d['grant_description'] = l_g[0]['grant_description']
+        writer.writerow(d)
 
 
 if __name__ == "__main__":
