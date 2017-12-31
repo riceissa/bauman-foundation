7e140a157c0a7f137f549ee644bceaaacaba347e
Issa Rice <riceissa@gmail.com>
2017-12-30 20:00:27 -0800

    fix this up to do sum checking

--- scrape.py
+++ scrape.py
@@ -15,53 +15,57 @@ def main():
 
 
 def go(writer):
-    # This is a map:
-    # int -> [{fiscal_year, amount, grant_type, grant_description} -> values]
-    grantee_dict = {}
+    # This is a map: int -> fiscal_year -> float
+    fy_sums = {}
 
-    all_grants_url = "https://www.baumanfoundation.org/grants/search?amount=All&fiscal_year=&name=&items_per_page=All"
-    response = requests.get(all_grants_url)
+    response = requests.get("https://www.baumanfoundation.org/grants/search?amount=All&fiscal_year=&name=&items_per_page=All")
     soup = BeautifulSoup(response.content, "lxml")
     rows = soup.find_all("tr")[1:]  # Skip the header row of the table
     for row in rows:
-        d = {}
         cols = row.find_all("td")
-        d['grantee'] = cols[0].text.strip()
+        grantee = cols[0].text.strip()
         grantee_path = cols[0].find("a")["href"]
         grantee_num = int(grantee_path.split('/')[-1])
-        d['grantee_url'] = ("https://www.baumanfoundation.org" +
+        grantee_url = ("https://www.baumanfoundation.org" +
                             grantee_path)
-        d['amount'] = cols[1].text.strip()
-        d['fiscal_year'] = cols[2].text.strip()
+        fiscal_year = cols[2].text.strip()
 
-        # Now we go to the grantee page to get more information about the
-        # grant, if we haven't done so already (the grantees are repeated in
-        # the main grants page, so it is possible we have already stored the
-        # grantee info, in which case we don't want to download the grantee
-        # page all over again)
-        if grantee_num not in grantee_dict:
-            grantee_dict[grantee_num] = []
-            response_g = requests.get(d['grantee_url'])
+        # This is the total for this (grantee, fiscal_year) combination, so
+        # it's different from a single grant. We store it to check against our
+        # per-grant sums (stored in fy_sums) to check our understanding of the
+        # site.
+        amount = cols[1].text.strip()
+        assert amount.startswith("$"), amount
+        amount = amount.replace("$", "").replace(",", "")
+
+        # Now we go to the grantee page to get information about each individual
+        # grant. We only need to do this once for each grantee.
+        if grantee_num not in fy_sums:
+            fy_sums[grantee_num] = {}
+            response_g = requests.get(grantee_url)
             soup_g = BeautifulSoup(response_g.content, "lxml")
             rows_g = soup_g.find_all("tr")[1:]  # Skip the header row of the table
             for row_g in rows_g:
-                d_g = {}
                 cols_g = row_g.find_all("td")
-                d_g['fiscal_year'] = cols_g[0].text.strip()
-                d_g['amount'] = cols_g[1].text.strip()
-                d_g['grant_type'] = cols_g[2].text.strip()
-                d_g['grant_description'] = cols_g[3].text.strip()
-                grantee_dict[grantee_num].append(d_g)
 
-        # Now, find the grant we are on among the grants on the grantee page
-        l_g = [x for x in grantee_dict[grantee_num]
-               if x['fiscal_year'] == d['fiscal_year'] and
-                  x['amount'] == d['amount']]
-        # There should be only one grant matching this
-        assert len(l_g) == 1, (d, grantee_dict[grantee_num], grantee_num, l_g)
-        d['grant_type'] = l_g[0]['grant_type']
-        d['grant_description'] = l_g[0]['grant_description']
-        writer.writerow(d)
+                d = {}
+                d['grantee'] = grantee
+                d['grantee_url'] = grantee_url
+                d['fiscal_year'] = cols_g[0].text.strip()
+                d['amount'] = cols_g[1].text.strip()
+                d['grant_type'] = cols_g[2].text.strip()
+                d['grant_description'] = cols_g[3].text.strip()
+                writer.writerow(d)
+
+                amount_g = d['amount']
+                assert amount_g.startswith("$")
+                amount_g = amount_g.replace("$", "").replace(",", "")
+                if d['fiscal_year'] not in fy_sums[grantee_num]:
+                    fy_sums[grantee_num][d['fiscal_year']] = amount_g
+                else:
+                    fy_sums[grantee_num][d['fiscal_year']] += amount_g
+
+        assert fy_sums[grantee_num][fiscal_year] == amount
 
 
 if __name__ == "__main__":
