49216d06d9992d9fcc179e4dc1f1e471cba06caa
Issa Rice <riceissa@gmail.com>
2017-12-30 22:00:35 -0800

    add filler

---
+++ proc.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+# License: CC0 https://creativecommons.org/publicdomain/zero/1.0/
+
+import csv
+import re
+
+
+def mysql_quote(x):
+    '''
+    Quote the string x using MySQL quoting rules. If x is the empty string,
+    return "NULL". Probably not safe against maliciously formed strings, but
+    whatever; our input is fixed and from a basically trustable source..
+    '''
+    if not x:
+        return "NULL"
+    x = x.replace("\\", "\\\\")
+    x = x.replace("'", "''")
+    x = x.replace("\n", "\\n")
+    return "'{}'".format(x)
+
+
+def main():
+    with open("data.csv", "r") as f:
+        reader = csv.DictReader(f)
+        first = True
+
+        print("""insert into donations (donor, donee, amount, donation_date,
+        donation_date_precision, donation_date_basis, cause_area, url,
+        donor_cause_area_url, notes, affected_countries,
+        affected_regions) values""")
+
+        for row in reader:
+            print(("    " if first else "    ,") + "(" + ",".join([
+                mysql_quote("MacArthur Foundation"),  # donor
+                mysql_quote(row['grantee']),  # donee
+                amount,  # amount
+                mysql_quote(donation_date),  # donation_date
+                mysql_quote("year"),  # donation_date_precision
+                mysql_quote("donation log"),  # donation_date_basis
+                mysql_quote("FIXME"),  # cause_area
+                mysql_quote("https://www.macfound.org/grants/"),  # url
+                mysql_quote("FIXME"),  # donor_cause_area_url
+                mysql_quote(row['notes']),  # notes
+                mysql_quote("FIXME"),  # affected_countries
+                mysql_quote(row['location']),  # affected_regions
+            ]) + ")")
+            first = False
+        print(";")
+
+
+if __name__ == "__main__":
+    main()
